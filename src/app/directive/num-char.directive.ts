981f343b783e472348080e92a46f0246aaa3c74b
Andrea Rota <104698199+arota18@users.noreply.github.com>
2025-03-27 15:28:24 +0100

    aggiunto default alla directive

--- src/app/directive/num-char.directive.ts
+++ src/app/directive/num-char.directive.ts
@@ -17,7 +17,15 @@ import {
   ],
 })
 export class NumCharDirective implements Validator {
-  @Input() appNumChar: string = "";
+  private _appNumChar!: string;
+  @Input("appNumChar")
+  set appNumChar(value: string) {
+    this._appNumChar = value || "rossi";
+  }
+  get appNumChar(): string {
+    return this._appNumChar;
+  }
+  // in alternativa è possibile mantenere l'Input e passare da un costruttore per valorizzare appNumChar
 
   validate(control: AbstractControl<string>): ValidationErrors | null {
     if (control.value === null) return null;
@@ -25,8 +33,4 @@ export class NumCharDirective implements Validator {
       ? null
       : { appNumChar: { errore: "Deve essere compreso tra 2 e 6 caratteri" } };
   }
-
-  // registerOnValidatorChange?(fn: () => void): void {
-  //   throw new Error('Method not implemented.');
-  // }
 }
